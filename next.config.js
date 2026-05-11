b09080b0ac0c7f571ff2461ccdea6a7c28f011d5
Rod <grecorodrigop@gmail.com>
2026-05-11 08:17:25 -0300

    Fix next.config.js: migrate serverComponentsExternalPackages to serverExternalPackages

--- next.config.js
+++ next.config.js
@@ -1,14 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-    serverComponentsExternalPackages: ['@supabase/supabase-js'],
-      },
+        serverExternalPackages: ['@supabase/supabase-js'],
         images: {
-    remotePatterns: [
-{ protocol: 'https', hostname: '*.supabase.co' },
-      { protocol: 'https', hostname: 'graph.facebook.com' },
-          ],
-      },
-      }
+                    remotePatterns: [
+                        { protocol: 'https', hostname: '*.supabase.co' },
+                        { protocol: 'https', hostname: 'graph.facebook.com' },
+                                ],
+        },
+}
 
-      module.exports = nextConfig
+module.exports = nextConfig
